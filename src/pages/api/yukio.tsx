147240aeda6d4edae63c2d539196a3ec0e4a4a2b
Rob Csaszar <rob.csaszar@gmail.com>
2022-11-27 21:34:33 +0200

    🚀 initial commit

---
+++ src/pages/api/yukio.tsx
@@ -0,0 +1,127 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { ImageResponse } from "@vercel/og";
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+const interFont = fetch(
+  new URL("../../assets/fonts/Inter-Bold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+const tornApi = `https://api.torn.com/user/?selections=profile&key=${process.env.TORN_API_KEY}`;
+
+interface TornResponse {
+  rank: string;
+  level: number;
+  gender: string;
+  property: string;
+  signup: string;
+  awards: number;
+  friends: number;
+  enemies: number;
+  forum_posts: number;
+  karma: number;
+  age: number;
+  role: string;
+  donator: 1;
+  player_id: number;
+  name: string;
+  property_id: number;
+  competition: null;
+  revivable: number;
+  life: {
+    current: number;
+    maximum: number;
+    increment: number;
+    interval: number;
+    ticktime: number;
+    fulltime: number;
+  };
+  status: {
+    description: string;
+    details: string;
+    state: string;
+    color: string;
+    until: number;
+  };
+  job: {
+    position: string;
+    company_id: number;
+    company_name: string;
+    company_type: number;
+  };
+  faction: {
+    position: string;
+    faction_id: number;
+    days_in_faction: number;
+    faction_name: string;
+    faction_tag: string;
+  };
+  states: {
+    hospital_timestamp: number;
+    jail_timestamp: number;
+  };
+  last_action: {
+    status: string;
+    timestamp: number;
+    relative: string;
+  };
+}
+
+export default async function handler() {
+  const inter = await interFont;
+  const torn: TornResponse = await fetch(tornApi).then((res) => res.json());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          backgroundImage: "url(http://localhost:3000/906148.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: 600,
+          height: 100,
+          color: "white",
+        }}
+      >
+        <div tw="flex flex-col absolute bg-white inset-2 rounded-lg p-2 w-1/3 bg-opacity-20">
+          <div tw="flex w-full items-center justify-between text-xl tracking-tight text-clip overflow-hidden">
+            <span tw="">{torn.name}</span>
+            <span tw="px-1 bg-white bg-opacity-20 rounded-md ml-2">
+              {torn.level}
+            </span>
+          </div>
+          <div tw="flex w-full items-center">
+            <span tw="text-sm opacity-75">{torn.rank}</span>
+          </div>
+          <div tw="flex w-full items-center">
+            <span tw="text-sm opacity-75">
+              {torn.last_action.status} · {torn.last_action.relative}
+            </span>
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: 600,
+      height: 100,
+      status: 200,
+      // debug: true,
+      headers: {
+        "Content-Type": "image/png",
+        "Cache-Control": "s-maxage=1, stale-while-revalidate",
+      },
+      fonts: [
+        {
+          data: inter,
+          name: "Inter",
+          style: "normal",
+        },
+      ],
+    }
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+function goBack(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+  e.preventDefault();
+  window.history.back();
+}
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>TORN Playground</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="flex flex-col items-center justify-center gap-12 px-4 py-16 text-center ">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white">
+            Caught you{" "}
+            <span className="text-[hsl(280,100%,70%)]">snoopin&apos;</span>
+          </h1>
+          <button
+            className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 font-bold text-white hover:bg-white/20"
+            onClick={goBack}
+          >
+            Go back →
+          </button>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;

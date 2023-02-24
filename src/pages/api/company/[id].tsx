1b0f5ad6d9548bf9f81eb01628f47ef921b5b975
Rob Csaszar <rob.csaszar@gmail.com>
2023-02-24 17:59:44 +0200

    removed empty file

--- src/pages/api/company/[id].tsx
+++ src/pages/api/company/[id].tsx
@@ -1,12 +1,10 @@
-/* eslint-disable @next/next/no-img-element */
-
 import type {
   CompanyInformation,
   EmployeeInformation,
 } from "../../../common/types";
-import type { NextRequest, NextResponse } from "next/server";
 
 import { ImageResponse } from "@vercel/og";
+import type { NextRequest } from "next/server";
 
 export const config = {
   runtime: "experimental-edge",

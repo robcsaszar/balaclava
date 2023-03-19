a505828ad63c14b994269973652874417707c5dd
Rob Csaszar <rob.csaszar@gmail.com>
2023-03-19 15:24:31 +0200

    change to gif

--- src/components/skully.tsx
+++ src/components/skully.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import SkullyImage from "@/assets/images/skully.png";
+import SkullyImage from "@/assets/images/skully.gif";
 
 export default function Skully({ width = 100, height = 100 }) {
   return (
